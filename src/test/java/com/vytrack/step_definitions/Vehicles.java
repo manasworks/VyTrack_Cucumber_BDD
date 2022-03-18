8888f885a6ecf111a3b438cbaf486f71838fa242
Manas Kalenov <manasworks@gmail.com>
2022-03-18 08:30:39 -0500

    Vytrack CY19_69 Checkboxes feature

--- src/test/java/com/vytrack/step_definitions/Vehicles.java
+++ src/test/java/com/vytrack/step_definitions/Vehicles.java
@@ -6,8 +6,10 @@ import com.vytrack.utils.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,7 @@ public class Vehicles {
         BrowserUtils.highlight(vehiclesPage.dots);
         actions.moveToElement(vehiclesPage.dots).perform();
     }
+
     @Then("user should see three buttons")
     public void user_should_see_three_buttons(List<String> expectedButtons) {
         List<String> actualButtons = new ArrayList<>();
@@ -33,5 +36,40 @@ public class Vehicles {
             actualButtons.add(each.getAttribute("title"));
         }
         Assert.assertEquals(expectedButtons, actualButtons);
+    }
+
+    @Then("user should see all the checkboxes are unchecked")
+    public void user_should_see_all_the_checkboxes_are_unchecked() {
+        vehiclesPage = new VehiclesPage();
+        for (WebElement checkbox : vehiclesPage.carCheckboxes) {
+            Assert.assertTrue(checkbox.isDisplayed());
+            Assert.assertFalse(checkbox.isSelected());
+        }
+    }
+
+    @Then("user click firs checkbox")
+    public void user_click_firs_checkbox() {
+        BrowserUtils.highlight(vehiclesPage.firstCheckbox);
+        vehiclesPage.firstCheckbox.click();
+    }
+
+    @Then("user should see all the chechboxes are checked")
+    public void user_should_see_all_the_chechboxes_are_checked() {
+        for (WebElement checkbox : vehiclesPage.carCheckboxes) {
+            Assert.assertTrue(checkbox.isSelected());
+        }
+    }
+
+    public int random = (int) (Math.random()*(20)) + 1;
+    public WebElement randomCheckbox = Driver.getDriver().findElement(By.xpath("//tr["+random+"]//td//input[@type='checkbox']"));
+
+    @Then("user click any checkbox")
+    public void user_click_any_checkbox() {
+        randomCheckbox.click();
+    }
+
+    @Then("user should see that checkbox is selected")
+    public void user_should_see_that_checkbox_is_selected() {
+        Assert.assertTrue(randomCheckbox.isSelected());
     }
 }
---
+++ src/test/resources/features/CY19_69_vehicle_checkboxes.feature
@@ -0,0 +1,25 @@
+@Regression @Smoke
+Feature: CY19-69 / As a user, I should be able to select any vehicle from the Vehicle page.
+
+  AC #1: once the user launched to the Vehicle page, all the checkboxes should be unchecked
+
+  AC #2: user check the first checkbox to check all the cars
+
+  AC #3: users can select any car
+
+  Accounts are: Drivers, Store Managers, Sales Managers
+
+  Scenario Outline: Users should be able to select any vehicle from the Vehicle page.
+    Given user '<username>' login to the Dashboard
+    When user on Dashboard page
+    And user navigates to "Fleet" module and clicks "Vehicles"
+    Then user should see all the checkboxes are unchecked
+    And user click firs checkbox
+    Then user should see all the chechboxes are checked
+    And user click firs checkbox
+    And user click any checkbox
+    Then user should see that checkbox is selected
+    Examples:
+      | username        |
+      | storemanager67  |
+      | salesmanager125 |
