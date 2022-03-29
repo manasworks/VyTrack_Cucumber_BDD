db086fd6263e151a1c0e8e08c641301d2cd80d50
Manas Kalenov <manasworks@gmail.com>
2022-03-29 07:57:34 -0500

    Vytrack Parallel Testing

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -6,13 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report/report.html",
-                  "rerun:target/rerun.txt",
-                  "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@Smoke",
         publish = false
 )
 
